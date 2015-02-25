c4ba5a13fb22121bfd43611ac83b971379b7c6d7
unknown <1231286@5B1-02.ad.dawsoncollege.qc.ca>
2015-02-24 19:59:04 -0500

    Completed Glasses Script and added Player script

---
+++ Assignment 2/Assets/Sciprt/Glasses.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glasses : MonoBehaviour {
+	
+	Player player;
+	public int Pre_min;
+	public int Pre_max;
+
+	int perscription()
+	{
+		return Random.Range(Pre_min,Pre_max);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		//generates the glasses perscription number then destroys self;
+		player = col.GetComponent<Player>();
+		player.perscription = perscription();
+		Destroy(this.gameObject);
+	}
+}
