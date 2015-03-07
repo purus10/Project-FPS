2864dffb93a518b561a7430658bd3c2a1b1f14c0
Kostantinos A <protecter_2@hotmail.com>
2015-03-07 04:51:04 -0500

    Added Bloom and anti-alias to the start scene.

--- Assignment 2/Assets/Sciprt/PressF.cs
+++ Assignment 2/Assets/Sciprt/PressF.cs
@@ -4,18 +4,42 @@ using System.Collections;
 public class PressF : MonoBehaviour {
 
 	public TextMesh pressf;
+	public GameObject cam;
+	Color o_color;
+	int t;
+	public bool fade = true;
+
+	void Start()
+	{
+		o_color = pressf.color;
+	}
 	
 	void Update () 
 	{
 		if (Player.reg_vision == false)
 		{
 			pressf.GetComponent<Renderer>().enabled = false;
+		}else{
+			if (fade == true && Player.reg_vision == true)
+			{
+				t++;
+				pressf.color = Color.Lerp(pressf.color,Color.clear,Time.time);
+				if (t == 55) fade = false;
+			}
+
+			if (fade == false && Player.reg_vision == true) 
+			{
+				t--;
+				pressf.color = Color.Lerp(pressf.color,Color.black,Time.time);
+				if (t == 0) fade = true;
+			}
 		}
 	}
 
+
 	void OnTriggerEnter()
 	{
-		Application.LoadLevel("FeatureTest");
+		Application.LoadLevel("SampleLevel");
 		Player.reg_vision = true;
 	}
 }
