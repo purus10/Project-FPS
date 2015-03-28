9a5f707ae3c045a2907e1158d3b118c2898101e0
Wilfred Young <goforit88@gmail.com>
2015-03-28 06:21:24 -0400

    Fixed a few things. Almost home!

---
+++ Assignment 2/Assets/Sciprt/OhNo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OhNo : MonoBehaviour {
+
+
+    public AudioSource UhOh;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
